5bb76d19ba9226842f488eaeb66d164f504c60b3
selinda2043 <selindaz@unc.edu>
2024-08-30 19:23:48 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,12 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730776458"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="Campers")
+greet(name="Selinda")
